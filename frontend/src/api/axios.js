e9246ddcaac9da7354ec0c8f9d405694c8b90b09
unknown <nishantskamble10@gmail.com>
2026-06-22 15:10:50 +0530

    Updated the Axios file

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://googleform-project.onrender.com'
+  baseURL: 'https://googleform-project.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
